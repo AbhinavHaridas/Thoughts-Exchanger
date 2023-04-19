3d4cd7a9e2b1c9917b8f27757bf12d28d64243ce
Abhinav <102739327+AbhinavHaridas@users.noreply.github.com>
2023-04-20 04:16:17 +0530

    Added login window

--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -3,8 +3,24 @@ import styles from '@/styles/Login.module.css';
 
 export default function Login() {
   return (
-    <div>
-        
-    </div>
+    <>
+      <div className={styles.page}>
+        <form className={styles.loginWindow}>
+          <label className={styles.label}>Enter Username </label>
+          <br />
+          <br />
+          <input type='text' className={styles.textInput} />
+          <br />
+          <br />
+          <br />
+          <label className={styles.label}>Enter Password </label>
+          <br />
+          <br />
+          <input type='password' className={styles.textInput} />
+          <div className={styles.submit}>submit</div>
+        </form>
+      </div>
+    </>
+
   )
 }
